c07bf040ef1f80a9c1e401b70bfd6f776aad6136
PinSopheaktra <sopheakpin342@gmail.com>
2025-10-21 19:07:24 +0700

    try fix repo

--- APinI/APinI/Repository/PinDataRepository.cs
+++ APinI/APinI/Repository/PinDataRepository.cs
@@ -8,25 +8,25 @@ namespace APinI.Repository
 {
     public class PinDataRepository
     {
+        private readonly IDbConnection GetConnection;
+        public PinDataRepository()
+        {
+            GetConnection = SqlConnection();
+        }
         private SqlConnection SqlConnection()
         {
             var cofig = System.Configuration.ConfigurationManager.ConnectionStrings["MyConnectionString"];
             var connectionstring = cofig?.ConnectionString ?? "Data Source=.;Initial Catalog=PinData;User Id=sa;Password=1234qwer";
             return new SqlConnection(connectionstring);
         }
-        private IDbConnection GetConnection => SqlConnection();
         public IEnumerable<T> GetData<T>(string spName)
         {
-            GetConnection.Open();
             var data = GetConnection.Query<T>(spName, null,null,true,null,CommandType.StoredProcedure);
-            GetConnection.Close();
             return data;
         } 
         public IEnumerable<T> GetData<T>(string spName,object param)
         {
-            GetConnection.Open();
             var data = GetConnection.Query<T>(spName, param, null, true, null, CommandType.StoredProcedure);
-            GetConnection.Close();
             return data;
         }
         internal List<GetAllTaskResponse> GetAllTask()

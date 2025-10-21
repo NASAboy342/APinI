049f7741de43d64d0c97b69691b144620417c0c3
PinSopheaktra <sopheakpin342@gmail.com>
2025-10-21 18:14:59 +0700

    fix duplicated ID

--- APinI/APinI/Services/IQOptionService.cs
+++ APinI/APinI/Services/IQOptionService.cs
@@ -18,5 +18,7 @@ public class IQOptionService : IIQOptionService
         {
             throw new ArgumentException("Candle list cannot be null or empty.");
         }
+        var groupedCandles = candles.GroupBy(c => c.Id);
+        candles = groupedCandles.Select(g => g.LastOrDefault() ?? new IQOptionCandle()).Where(c => c.Id != 0).ToList();
     }
 }

c55b09a49a479daf7b8d81887ed3a271f87e2141
NASAboy342 <sopheakpin342@gmail.com>
2025-12-05 20:46:52 +0700

    add api create user

---
+++ APinI/APinI/Controllers/SpendBookController.cs
@@ -0,0 +1,24 @@
+﻿using APinI.Models;
+using APinI.Models.SpendBook.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APinI.Controllers
+{
+    [ApiController]
+    [Route("api/spendbook")]
+    public class SpendBookController : ControllerBase
+    {
+
+        [HttpPost("create-user")]
+        public async Task<ApiBaseResponse<BaseResponse>> CreateUser(CreateUserRequest req)
+        {
+            return new ApiBaseResponse<BaseResponse>(new BaseResponse
+            {
+                ErrorCode = 0,
+                ErrorMessage = "User created successfully"
+            });
+        }
+
+
+    }
+}
---
+++ APinI/APinI/Models/ApiBaseResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace APinI.Models
+{
+    public class ApiBaseResponse<T> where T : BaseResponse
+    {
+        public ApiBaseResponse(T response)
+        {
+            Data = response;
+            ErrorCode = response.ErrorCode;
+            ErrorMessage = response.ErrorMessage;
+        }
+        public T Data { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}

3631d7aba8cc23d3f21315f3a46473619dcdc1bc
NASAboy342 <sopheakpin342@gmail.com>
2025-12-14 19:16:39 +0700

    add defualt when get transaction

--- APinI/APinI/Models/SpendBook/Requests/GetTransactionRequest.cs
+++ APinI/APinI/Models/SpendBook/Requests/GetTransactionRequest.cs
@@ -4,8 +4,8 @@
     {
         public string Username { get; set; } = string.Empty;
         public int AccountId { get; set; }
-        public DateTimeOffset FromUtcDate { get; set; }
-        public DateTimeOffset ToUtcDate { get; set; }
-        public int? TrackingTopicId { get; set; }
+        public DateTimeOffset FromUtcDate { get; set; } = DateTimeOffset.UtcNow.AddMonths(-1);
+        public DateTimeOffset ToUtcDate { get; set; } = DateTimeOffset.UtcNow;
+        public int? TrackingTopicId { get; set; } = 0;
     }
 }
